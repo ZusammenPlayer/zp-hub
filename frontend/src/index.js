b808ac2ed67ae670c9f0d5f8c366144ad9c48dbb
Markus Schubert <markus@toto.io>
2023-05-23 20:08:48 +0200

    add riot router to project and register route components globally

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,13 @@ import './style.css'
 import '@riotjs/hot-reload'
 import { mount } from 'riot'
 import registerGlobalComponents from './register-global-components'
+import { Route, Router } from '@riotjs/route'
+import { register } from 'riot'
 
 // register
 registerGlobalComponents()
+register('router', Router)
+register('route', Route)
 
 // mount all the global components found in this page
 mount('[data-riot-component]')
