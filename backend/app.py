e26af834b7b36af7814e9f3910a0d969eb98c343
Markus Schubert <markus@toto.io>
2023-05-25 01:18:48 +0200

    add database abstraction
    added a new class that abstracts all data persistence operations. app.py is less bloated. also config params zp_db_file_path and zp_projects_path are removed from the code. we only use zp_data_path and all other directories and file path are derived from that value

--- backend/app.py
+++ backend/app.py
@@ -6,108 +6,49 @@ import os
 from slugify import slugify
 import random
 import string
+from zp_database import ZP_Database as Database, DatabaseException
 
 ROOM_DEVICES = 'room_devices'
 ROOM_WEB = 'room_web_clients'
 
 connected_devices = []
-database = {
-    'version': 1,
-    'projects': []
-}
-current_project = []
+database = Database(config.zp_data_path)
 
-async def handle_api(request):
+# api endpoints
+
+async def get_all_devices(_):
     global connected_devices
-    print('request: ', request)    
-    data = json.dumps(connected_devices)
-    return web.Response(text=data)
+    return web.json_response(connected_devices)
 
-async def get_all_devices(request):
-    global connected_devices
-    data = json.dumps(connected_devices)
-    return web.Response(text=data)
-
-async def get_all_projects(request):
-    global database
-    data = []
-    for project in database['projects']:
-        data.append({
-            'id': project['id'],
-            'name': project['name'],
-            'slug': project['slug'],
-        })
-    return web.json_response(data)
+async def get_all_projects(_):
+    return web.json_response(database.get_projects())
 
 async def get_project(request):
-    global database
     q = request.query
     project = None
     if 'id' in q:
-        for item in database['projects']:
-            if item['id'] == q['id']:
-                project = item
+        project = database.get_project(q['id'])
     elif 'slug' in q:
-        for item in database['projects']:
-            if item['slug'] == q['slug']:
-                project = item
+        project = database.get_project_by_slug(q['slug'])
 
     if project != None:
-        file_name = project['file_name']
-        with open(file_name, "r") as outfile:
-            response_data = json.load(outfile)
-            del response_data['file_name']
-            return web.json_response(response_data)
+        return web.json_response(project)
     else:
         return web.HTTPNotFound()
 
 async def create_new_project(request):
-    global database
-
     request_data = await request.json()
-
-    # check wether a project with the specified name already exists
-    project_name_exists = False
-    for project in database['projects']:
-        if project['name'] == request_data['name']:
-            project_name_exists = True
-
-    if project_name_exists:
-        error = {
-            'code': 1,
-            'message': 'project with given name already exists'
-        }
-        return web.HTTPBadRequest(text=json.dumps(error), content_type="application/json")
-    else:
-        slug = slugify(request_data['name'])
-        id = 'project_' + ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-        file_name = config.zp_projects_path + '/' + id + '.json'
-
-        new_project = {
-            'id': id,
-            'name': request_data['name'],
-            'slug': slug,
-            'file_name': file_name,
-        }
-
-        out = json.dumps(new_project)
-
-        with open(file_name, "w") as outfile:
-            outfile.write(out)
-        database['projects'].append(new_project)
-        save_database()
-        del new_project['file_name']
+    try:
+        new_project = database.create_new_project(request_data)
         return web.json_response(new_project)
+    except DatabaseException as ex:
+        return web.HTTPBadRequest(text=ex.to_json(), content_type="application/json")
 
-
+# create web application 
 app = web.Application()
-
 sio = socketio.AsyncServer(cors_allowed_origin="*", async_mode='aiohttp')
 sio.attach(app)
-
-
 app.add_routes([
-    web.get('/api', handle_api),
     web.get('/api/project/all', get_all_projects),
     web.get('/api/project', get_project),
     web.post('/api/project', create_new_project),
@@ -115,7 +56,7 @@ app.add_routes([
     web.static('/', 'web-client', show_index=True, follow_symlinks=True),
 ])
 
-
+# socket io event handling
 @sio.event
 async def connect(sid, data):
     global connected_devices
@@ -151,46 +92,13 @@ async def disconnect(sid):
 
 
 def init():
-    global database
     print('Startup ZusammenPlayer Hub')
-
-    # check if data path exists, otherwise create it
-    if not os.path.exists(config.zp_data_path):
-        os.mkdir(config.zp_data_path)
-        print('created zp data directory')
-
-    # check if projects directory exists otherwise create one
-    if not os.path.exists(config.zp_projects_path):
-        os.mkdir(config.zp_projects_path)
-        print('created zp projects directory')
-    
-    # check if database file exists otherwise create one
-    if not os.path.exists(config.zp_db_file_path):
-        with open(config.zp_db_file_path, "w") as outfile:
-            outfile.write(json.dumps(database))
-            print('created zp database file')
-    
-    # check if data file is a file
-    if not os.path.isfile(config.zp_db_file_path):
-        print('error: database file is a directory')
+    try:
+        database.init()
+    except DatabaseException as ex:
+        print("could not create database: ", ex.message)
         return False
-    
-    # check if project dir is a directory
-    if not os.path.isdir(config.zp_projects_path):
-        print('projects path is a file but should be a directory')
-        return False
-    
-    # load projects
-    with open(config.zp_db_file_path) as db:
-        database = json.load(db)
-
     return True
-
-def save_database():
-    global database
-    with open(config.zp_db_file_path, "w") as outfile:
-        outfile.write(json.dumps(database))
-        print('database saved')
 
 init()
 
--- backend/config.template.py
+++ backend/config.template.py
@@ -1,3 +1,1 @@
 zp_data_path = '<your path to data dir>'
-zp_db_file_path = zp_data_path + '/db.json'
-zp_projects_path = zp_data_path + '/projects'
