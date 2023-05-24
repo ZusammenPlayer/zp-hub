17cad3628cd1f3786d8bfb9edb75d4ccd2a65213
… <fricklr>
2023-05-25 01:08:18 +0200

    assets local routing first working prototype closes #11

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,12 @@ import '@riotjs/hot-reload'
 import { component } from 'riot'
 import App from './app.riot'
 import registerGlobalComponents from './register-global-components'
+import './components/assets/css/style.css'
+import './components/assets/css/googlefonts.css'
+import './components/assets/css/normalize.css'
+import './components/assets/css/milligram.css'
+import './components/assets/js/socket.io.min.js'
+
 
 // register
 registerGlobalComponents()
