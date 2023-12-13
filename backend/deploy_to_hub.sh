76281aac2bcfe6d2ed8b52082f596525161c9336
Markus Schubert <markus@toto.io>
2023-12-13 17:36:43 +0100

    cleanup deployment script

--- backend/deploy_to_hub.sh
+++ backend/deploy_to_hub.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 scp app.py zpadmin@zp-hub:/opt/zp-hub/backend
-scp blob_storage.py zpadmin@zp-hub:/opt/zp-hub/backend
 scp data_utils.py zpadmin@zp-hub:/opt/zp-hub/backend
-scp zp_database.py zpadmin@zp-hub:/opt/zp-hub/backend
+scp database.py zpadmin@zp-hub:/opt/zp-hub/backend
 scp requirements.txt zpadmin@zp-hub:/opt/zp-hub/backend
