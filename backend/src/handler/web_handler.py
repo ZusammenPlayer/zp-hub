240791e2ede0dd25c81475fe2fdf0e5bb63240ce
fricklr <post@fricklr.de>
2023-12-14 01:47:06 +0100

    fix deep linking routing in web_handler

--- backend/src/handler/web_handler.py
+++ backend/src/handler/web_handler.py
@@ -9,6 +9,7 @@ def setup(app: web.Application):
     app.router.add_get("/api/debug/ping", debug_ping)
     app.router.add_post("/api/debug/play", debug_play)
     app.router.add_get("/", index_handler)
+    app.router.add_route('GET', '/{tail:(?!api).*}', index_handler)
     app.router.add_static(
         "/", config.zp_web_client_path, show_index=True, follow_symlinks=True
     )
